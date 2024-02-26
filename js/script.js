e644e35c06a04391180a3912ca2f0e134c0a0460
spigkaht <benj@betterrentals.com.au>
2024-02-27 09:05:18 +1100

    Added function and dummy content to array

--- js/script.js
+++ js/script.js
@@ -1,3 +1,24 @@
+const myLibrary = [
+  {
+    title: "Lord of the Rings",
+    author: "J.R.R. Tolkien",
+    pages: 1178,
+    read: true,
+  },
+  {
+    title: "Wheel of Time",
+    author: "Robert Jordan",
+    pages: 11898,
+    read: false,
+  },
+  {
+    title: "Pawn of Prophecy",
+    author: "David Eddings",
+    pages: 278,
+    read: true,
+  },
+];
+
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
@@ -7,3 +28,11 @@ function Book(title, author, pages, read) {
     return this;
   };
 }
+
+function addBookToLibrary() {}
+
+function displayBook(library) {
+  library.forEach((el) => console.log(el));
+}
+
+displayBook(myLibrary);
