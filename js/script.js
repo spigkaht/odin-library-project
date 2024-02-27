fd71f6e684f191ec6d39d63d0173e6d6303406ad
spigkaht <benj@betterrentals.com.au>
2024-02-28 09:51:40 +1100

    Found error I cannot figure out (see below)
    Will attempt debugging with breakpoints,
    otherwise will redo file.
    
    document.querySelector(#library) is returning
    Null for some reason

--- js/script.js
+++ js/script.js
@@ -32,7 +32,32 @@ function Book(title, author, pages, read) {
 function addBookToLibrary() {}
 
 function displayBook(library) {
-  library.forEach((el) => console.log(el));
+  library.forEach((el) => {
+    console.log(el);
+
+    const container = document.querySelector("#library");
+    console.log(container);
+    const tableRow = document.createElement("tr");
+    container.appendChild(tableRow);
+
+    for(i = 0; i < library.length; i++) {
+      const tableCellTitle = document.createElement("td");
+      tableCellTitle.textContent = el.title;
+      tableRow.appendChild(tableCellTitle);
+      const tableCellAuthor = document.createElement("td");
+      tableCellAuthor.textContent = el.author;
+      tableRow.appendChild(tableCellAuthor);
+      const tableCellPages = document.createElement("td");
+      tableCellPages.textContent = el.pages;
+      tableRow.appendChild(tableCellPages);
+      const tableCellRead = document.createElement("td");
+      tableCellRead.textContent = el.read;
+      tableRow.appendChild(tableCellRead);
+    }
+  
+    console.log(container, tableRow);
+    container.appendChild(tableRow);
+  });
 }
 
 displayBook(myLibrary);
