364787f70ba8189a870e6d7f7c7c7f38a9f80d1f
spigkaht <benj@betterrentals.com.au>
2024-02-28 12:26:47 +1100

    Fixed issue with table / querySelector

--- js/script.js
+++ js/script.js
@@ -33,30 +33,18 @@ function addBookToLibrary() {}
 
 function displayBook(library) {
   library.forEach((el) => {
-    console.log(el);
+    const tableEl = document.querySelector("tbody");
 
-    const container = document.querySelector("#library");
-    console.log(container);
-    const tableRow = document.createElement("tr");
-    container.appendChild(tableRow);
+    el.read ? el.read = "Yes" : el.read = "No";
 
-    for(i = 0; i < library.length; i++) {
-      const tableCellTitle = document.createElement("td");
-      tableCellTitle.textContent = el.title;
-      tableRow.appendChild(tableCellTitle);
-      const tableCellAuthor = document.createElement("td");
-      tableCellAuthor.textContent = el.author;
-      tableRow.appendChild(tableCellAuthor);
-      const tableCellPages = document.createElement("td");
-      tableCellPages.textContent = el.pages;
-      tableRow.appendChild(tableCellPages);
-      const tableCellRead = document.createElement("td");
-      tableCellRead.textContent = el.read;
-      tableRow.appendChild(tableCellRead);
-    }
-  
-    console.log(container, tableRow);
-    container.appendChild(tableRow);
+    tableEl.innerHTML += `
+      <tr>
+        <td>${el.title}</td>
+        <td>${el.author}</td>
+        <td>${el.pages}</td>
+        <td>${el.read}</td>
+      </tr>
+    `;
   });
 }
 
